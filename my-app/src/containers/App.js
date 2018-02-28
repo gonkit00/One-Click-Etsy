9c3ac0e5f4c264f027783b288d89f3f52a07de3e
gonkit00 <m.espo@tiscali.it>
2018-02-28 10:27:25 +0100

    limit and offset variables added

--- my-app/src/containers/App.js
+++ my-app/src/containers/App.js
@@ -15,14 +15,17 @@ class App extends Component {
     const baseUrl = 'https://openapi.etsy.com/v2';
     const shop = 'lollycloth';
     const apiKey = 'qby4xtftjcqygc06vwpk5oom';
+    const limit = 30;
+    const offset = 0;
     //TODO: manage the limit/pagination
     //TODO: manage cors
-    fetch(`${baseUrl}/shops/${shop}/listings/active?api_key=${apiKey}`)
+    fetch(`${baseUrl}/shops/${shop}/listings/active?limit=${limit}&offset=${offset}&&api_key=${apiKey}`)
       .then(res => res.json())
       .then(response => this.props.addListings(response.results))
   }
 
   render() {
+    console.log('Fetch in App: ', this.props.listings);
     return (
       <div className="App">
         <ListingList listings={this.props.listings}/>
