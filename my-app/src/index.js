6322a639a71e3dc3c75e1980e9dc53747ef31e81
gonkit00 <m.espo@tiscali.it>
2018-03-03 18:29:42 +0100

    muitheme set up in App.js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -8,13 +8,36 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducer from './reducers';
 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import {grey600} from 'material-ui/styles/colors';
+import {deepOrange400} from 'material-ui/styles/colors';
+import {cyan400} from 'material-ui/styles/colors';
+
+
+// This replaces the textColor value on the palette
+// and then update the keys for each component that depends on it.
+// More on Colors: http://www.material-ui.com/#/customization/colors
+const muiTheme = getMuiTheme({
+  palette: {
+    textColor: grey600,
+    primary1Color: cyan400,
+    accent1Color: deepOrange400,
+  },
+  App: {
+    // height: 50,
+  },
+});
+
 
 let store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ &&
   window.__REDUX_DEVTOOLS_EXTENSION__());
 
   ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <MuiThemeProvider muiTheme={muiTheme}>
+      <App />
+    </MuiThemeProvider>
   </Provider>,
   document.getElementById('root')
 );
