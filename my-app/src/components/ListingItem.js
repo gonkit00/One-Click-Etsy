11abb3cf283806263d5ff04b5e3e569f88eca934
gonkit00 <m.espo@tiscali.it>
2018-03-03 18:49:54 +0100

    listingItem updated

--- my-app/src/components/ListingItem.js
+++ my-app/src/components/ListingItem.js
@@ -1,27 +1,64 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import RaisedButton from 'material-ui/RaisedButton';
 import './ListingItem.css';
 
-const ListingItem = (props) => {
-  return (
-    <div className="ListingItem">
-      <a
-        key={props.listing.listing_id}
-        href="#"
-        //When I have parameters to pass use a callback!
-        onClick={() => {props.onSelectListing(props.listing)}}
-      >
-        <img
-          src={props.listing.Images[0].url_170x135}
-          alt={props.listing.title.substr(0,80)}
-        />
-        <div>
-          <small className="description">
-            {props.listing.title.substr(0,80)}...
-          </small>
-        </div>
-      </a>
-    </div>
-  )
+class ListingItem extends Component {
+
+  cardStyle = {
+    width: 200
+  };
+  // buttonStyle = {
+  //   marginLeft: 45
+  // };
+  state = {
+    cardClass: ''
+  }
+
+  handleClick = () => {
+    if (this.state.cardClass !== '') {
+      this.setState({cardClass: ''})
+    } else {
+      this.setState({cardClass: 'selectedCard'})
+    }
+    this.props.onSelectListing(this.props.listing)
+  }
+
+  render () {
+    return (
+      <div className="ListingItem">
+        <Card className={this.state.cardClass} style={this.cardStyle} onClick={this.handleClick} >
+          <CardMedia>
+            <img src={this.props.listing.Images[0].url_170x135} alt={this.props.listing.title.substr(0,80)} />
+          </CardMedia>
+          <CardTitle
+            subtitle={this.props.listing.title.split(' ').filter((el, i) => {
+            return i < 3
+            }).join(' ') + '...'}
+          />
+          {/* <CardActions>
+            <RaisedButton label="Select" primary={true} style={this.buttonStyle} onClick={() => {this.props.onSelectListing(this.props.listing)}} />
+          </CardActions> */}
+        </Card>
+        {/* <a
+          key={this.props.listing.listing_id}
+          href="#"
+          //When I have parameters to pass use a callback!
+          onClick={() => {this.props.onSelectListing(this.props.listing)}}
+        >
+          <img
+            src={this.props.listing.Images[0].url_170x135}
+            alt={this.props.listing.title.substr(0,80)}
+          />
+          <div>
+            <small className="description">
+              {this.props.listing.title.substr(0,80)}...
+            </small>
+          </div>
+        </a> */}
+      </div>
+    )
+  }
 }
 
 export default ListingItem
