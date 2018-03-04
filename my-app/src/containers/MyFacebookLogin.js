719979f52ce046d984cd331ab92cf8e9bb8dcf06
gonkit00 <m.espo@tiscali.it>
2018-03-04 15:40:17 +0100

    facebook button login is centered and working with popups

--- my-app/src/containers/MyFacebookLogin.js
+++ my-app/src/containers/MyFacebookLogin.js
@@ -11,75 +11,21 @@ class MyFacebookLogin extends Component {
 
   //TODO: MyFacebookLogin: when facebook login decide what is the privacy of my posts
   //TODO: MyFacebookLogin: empty the array after publishing
-  //TODO: post for Facebook pages
+  //TODO: post for Facebook PAGES
   //TODO: recognize &#39; &quot; etc characters in description
-  //TODO: handle facebooklogin button visibility
 
   state = {facebookToken:undefined}
-
-  // baseUrl = 'https://graph.facebook.com/v2.12';
 
   responseFacebook = (response) => {
     this.setState({facebookToken:response.accessToken})
   }
 
-  // publishPost = () => {
-  //   this.props.selectedListings.forEach(listing => {
-  //     //promises is an array of promises
-  //     const promises = listing.Images.map(image =>
-  //       fetch(`${this.baseUrl}/me/photos?url=${image.url_570xN}&caption=my caption&published=false&access_token=${this.state.facebookToken}`, {
-  //         method: "POST"
-  //       })
-  //       .then(res => res.json())
-  //       .then(res => res.id)
-  //     )
-  //
-  //     const description = listing.description.split('.').filter((el, index) => {
-  //       return index < 3
-  //     }).join('.');
-  //
-  //     const tags = listing.tags.filter((el, index) => {
-  //       return index < 3
-  //     }).map(tag => {return '#'+tag.split(' ').join('')}).join(' ');
-  //
-  //     let message = {message: description + '\n\n' + tags};
-  //     message = queryString.stringify(message, {encode: true});
-  //
-  //     // fetch(`${this.baseUrl}/me/feed
-  //     //   ?message=Hello Kimba!
-  //     //   &attached_media[0]={"media_fbid":"${res.id}"}
-  //     //   &attached_media[1]={"media_fbid":"${res.id}"}
-  //     //   &attached_media[2]={"media_fbid":"${res.id}"}
-  //     //   &access_token=${this.props.facebookToken}`, {
-  //     //   method: "POST"
-  //     // })
-  //     //INSTEAD OF THE SCRIPT ABOVE WE USE THE FOLLOWING
-  //     Promise.all(promises)
-  //     .then (ids => {
-  //       const qs = ids.map((id, index) => `attached_media[${index}]={"media_fbid":"${id}"}`).join('&');
-  //
-  //       fetch(`${this.baseUrl}/me/feed?${message}&${qs}&access_token=${this.state.facebookToken}`,
-  //         {
-  //           method: "POST"
-  //         })
-  //         .then(res => console.log('Facebook Post result: ',res.ok))
-  //     })
-  //   })
-  // }
-
   render () {
-
-    // const style = {
-    //   height: 70,
-    //   width: 350,
-    //   alignSelf: "center",
-    // };
 
     if (this.props.selectedListings.length !== 0 && !this.state.facebookToken) {
 
       return (
-        //This is a workaround: check TODO
-        <div className="displayNone">
+        <div className="MyFacebookLogin">
           <FacebookLogin
             appId={config.facebookAppId}
             autoLoad={true}
