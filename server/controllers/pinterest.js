c2688154376217d802671a92c6ce88b3f3584649
gonkit00 <m.espo@tiscali.it>
2018-03-05 19:03:25 +0100

    ngrok installed

--- server/controllers/pinterest.js
+++ server/controllers/pinterest.js
@@ -1,8 +1,10 @@
 const OAuth = require('oauth')
 
-exports.reqToken = ctx => {
+exports.reqToken = async ctx => {
   try {
-ctx.body = "Kimba!"
+    const OAuth2 = OAuth.OAuth2;
+    const pinterestAppId = '4954023344243486938';
+    const pinterestAppSecret = 'aa7e417fdcbbfe43806bba9cd16938f75a334b56656865c4bf448549f8b56be3'
 
   } catch (e) {
     ctx.status = 500
